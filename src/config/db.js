21fe658a4446d4c6d13d0cdb96b01bb08e488575
Sakha_Naufal_Huda <sakhanaufal2@gmail.com>
2025-09-07 20:37:18 +0700

    bug: change connection db

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,21 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectDB = async () => {
+    if (isConnected) {
+        return;
+    }
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
+        isConnected = conn.connections[0].readyState;
         console.log(`📦 MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`❌ Error: ${error.message}`);
-        process.exit(1);
+        throw error;
     }
 };
 
