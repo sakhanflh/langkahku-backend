a97cd3931ab5a22318747c533b901812d701c2d3
Sakha_Naufal_Huda <sakhanaufal2@gmail.com>
2025-09-25 12:46:41 +0700

    add progress on goalsModal

--- src/models/goalsModel.js
+++ src/models/goalsModel.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const goalsSchema = new mongoose.Schema({
     judul: { type: String, required: true },
     deskripsi: { type: String },
+    progress: {type: Number, default: 0},
     targetTanggal: { type: Date },
     tercapai: { type: Boolean, default: false },
     dibuat: { type: Date, default: Date.now },
