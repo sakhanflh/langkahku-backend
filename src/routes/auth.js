245304b705cf0692ad101cd7dd19e8c784e554a5
Sakha_Naufal_Huda <sakhanaufal2@gmail.com>
2025-11-02 12:30:38 +0700

    fix: bug on endpoint regist

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { login } from "../controllers/authController.js";
+import { login, register } from "../controllers/authController.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
+router.post("/register", register);
 router.post("/login", login);
 
 // Check session
